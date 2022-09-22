f323529c10221215929aa6cd485428790a33d649
Andrei Filimon <andrei.filimon@nordcloud.com>
2022-09-22 19:56:24 +0300

    search me poc

---
+++ WindowsFileExplorerAutomation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Automation;
+using System.Windows.Forms;
+
+namespace WindowsFileExplorerAutomation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ieWindow = AutomationElement.RootElement.FindFirst(TreeScope.Children, new
+ PropertyCondition(AutomationElement.ClassNameProperty, "CabinetWClass"));
+
+            if (ieWindow == null)
+                return;
+
+            var searchBox = ieWindow.FindFirst(TreeScope.Subtree, new PropertyCondition(AutomationElement.NameProperty, "SearchEditBox"));
+            if (searchBox == null)
+                return;
+            if (!searchBox.Current.IsEnabled)
+                return;
+            ieWindow.SetFocus();
+            var pattern= (ValuePattern)searchBox.GetCurrentPattern(ValuePattern.Pattern);
+            pattern.SetValue("");
+            SendKeys.SendWait("search me");
+        }
+    }
+}
